79c5e2efd9c1f9dc9480baadb66368b73370048f
sstefdev <sstefandev@gmail.com>
2024-02-24 04:52:10 +0100

    removed env from workflow

--- src/utils/axios/config.ts
+++ src/utils/axios/config.ts
@@ -1,9 +1,14 @@
+const NEXT_PUBLIC_MOBULA_API = "73a027f6-a4cd-45af-9c8f-15cce33ee67a";
+const NEXT_PUBLIC_CRYPTOCOMPARE_URI = "https://min-api.cryptocompare.com/data/";
+const NEXT_PUBLIC_CRYPTOCOMPARE_KEY =
+  "f38a5324d3158724386080bd8577ffed865f2817874798d2f000b6716bbde627";
+
 export const apiConfig = {
   mobula: {
-    key: process.env.NEXT_PUBLIC_MOBULA_API,
+    key: NEXT_PUBLIC_MOBULA_API,
   },
   cryptoCompare: {
-    url: process.env.NEXT_PUBLIC_CRYPTOCOMPARE_URI,
-    key: process.env.NEXT_PUBLIC_CRYPTOCOMPARE_KEY,
+    url: NEXT_PUBLIC_CRYPTOCOMPARE_URI,
+    key: NEXT_PUBLIC_CRYPTOCOMPARE_KEY,
   },
 };
