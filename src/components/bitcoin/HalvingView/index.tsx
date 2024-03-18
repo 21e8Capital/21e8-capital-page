57cc616a2ded7784ae63549497f8a23b4a5f4ecd
sstefdev <sstefandev@gmail.com>
2024-03-18 14:22:17 +0100

    fix: no halving data handle

--- src/components/bitcoin/HalvingView/index.tsx
+++ src/components/bitcoin/HalvingView/index.tsx
@@ -6,7 +6,6 @@ import styles from "./styles.module.scss";
 const HalvingView = () => {
   const { theme } = useTheme();
   const { halving } = useContextState();
-  const { countdown, inflation, emission } = halving;
 
   const gradientImage =
     theme === "light"
@@ -17,9 +16,9 @@ const HalvingView = () => {
     <div className={styles.halving}>
       <h2>Halving Countdown</h2>
       <div className={styles.halvingGrid}>
-        {countdown?.countdownTime ? (
+        {halving?.countdown?.countdownTime ? (
           <h3>
-            {countdown?.countdownTime?.slice(0, 2)}{" "}
+            {halving?.countdown?.countdownTime?.slice(0, 2)}{" "}
             <span>
               Days <br />
               to go
@@ -28,41 +27,41 @@ const HalvingView = () => {
         ) : (
           <Skeleton width={240} height={100} />
         )}
-        {emission?.currentEmission ? (
+        {halving?.emission?.currentEmission ? (
           <h3>
-            {`${emission?.currentEmission}`}
+            {`${halving?.emission?.currentEmission}`}
             <span>Current Daily Emission</span>
           </h3>
         ) : (
           <Skeleton width={240} height={100} />
         )}
-        {emission?.forecastedEmission ? (
+        {halving?.emission?.forecastedEmission ? (
           <h3>
-            {`${emission?.forecastedEmission}`}
+            {`${halving?.emission?.forecastedEmission}`}
             <span>Next Daily Emission</span>
           </h3>
         ) : (
           <Skeleton width={240} height={100} />
         )}
-        {countdown?.halveningDate ? (
+        {halving?.countdown?.halveningDate ? (
           <h3>
-            {countdown?.halveningDate?.day}{" "}
-            <span>{countdown?.halveningDate?.restOfDate}</span>
+            {halving?.countdown?.halveningDate?.day}{" "}
+            <span>{halving?.countdown?.halveningDate?.restOfDate}</span>
           </h3>
         ) : (
           <Skeleton width={240} height={100} />
         )}
-        {inflation?.currentInflationRate ? (
+        {halving?.inflation?.currentInflationRate ? (
           <h3>
-            {`${inflation?.currentInflationRate}%`}
+            {`${halving?.inflation?.currentInflationRate}%`}
             <span className={styles.smallTxt}>Current Inflation Rate</span>
           </h3>
         ) : (
           <Skeleton width={240} height={100} />
         )}
-        {inflation?.nextInfationRate ? (
+        {halving?.inflation?.nextInfationRate ? (
           <h3>
-            {`${inflation?.nextInfationRate}%`}
+            {`${halving?.inflation?.nextInfationRate}%`}
             <span className={styles.smallTxt}>Next Inflation Rate</span>
           </h3>
         ) : (
