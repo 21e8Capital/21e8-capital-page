3ce60d256a338290bb3c07d64ac3a288e56a31da
sstefdev <sstefandev@gmail.com>
2024-04-24 17:57:09 +0200

    un commentdefi and bitcoin page

--- src/pages/bitcoin.tsx
+++ src/pages/bitcoin.tsx
@@ -34,149 +34,143 @@ const etfTrackerConfig = {
   lineLabel: "Total Net BTS flows to ETFs",
 };
 
-const PriceData = () => {
-  // const [imagesToDownload, setImagesToDownload] = useState<
-  //   { [key: string]: string }[]
-  // >([]);
+const PriceData = ({
+  etfData,
+  marketStats,
+  performance,
+  stockToFlow,
+  googleTrends,
+}: PriceDataProps) => {
+  const [imagesToDownload, setImagesToDownload] = useState<
+    { [key: string]: string }[]
+  >([]);
 
-  // const shareLinks = {
-  //   bitcoinMarketStats: {
-  //     url: `${window.location.href}#market-stats`,
-  //     title: "21e8.Capital - Bitcoin Market Stats",
-  //   },
-  //   bitcoinPerformance: {
-  //     url: `${window.location.href}#performance`,
-  //     title: "21e8.Capital - Bitcoin Performance",
-  //   },
-  //   etfTracker: {
-  //     url: `${window.location.href}#etf-tracker`,
-  //     title: "21e8.Capital - Bitcoin Etf Tracker",
-  //   },
-  //   stockToFlow: {
-  //     url: `${window.location.href}#stock-to-flow`,
-  //     title: "21e8.Capital - Bitcoin Stock To Flow",
-  //   },
-  //   interestOverTime: {
-  //     url: `${window.location.href}#interest-over-time`,
-  //     title: "21e8.Capital - Bitcoin Interest Over Time",
-  //   },
-  // };
+  const shareLinks = {
+    bitcoinMarketStats: {
+      url: `${window.location.href}#market-stats`,
+      title: "21e8.Capital - Bitcoin Market Stats",
+    },
+    bitcoinPerformance: {
+      url: `${window.location.href}#performance`,
+      title: "21e8.Capital - Bitcoin Performance",
+    },
+    etfTracker: {
+      url: `${window.location.href}#etf-tracker`,
+      title: "21e8.Capital - Bitcoin Etf Tracker",
+    },
+    stockToFlow: {
+      url: `${window.location.href}#stock-to-flow`,
+      title: "21e8.Capital - Bitcoin Stock To Flow",
+    },
+    interestOverTime: {
+      url: `${window.location.href}#interest-over-time`,
+      title: "21e8.Capital - Bitcoin Interest Over Time",
+    },
+  };
 
-  // useEffect(() => {
-  //   const handleImageSaving = async () => {
-  //     const imageIds = [
-  //       "market-stats",
-  //       "performance",
-  //       "etf-tracker",
-  //       "stock-to-flow",
-  //       "interest-over-time",
-  //     ];
+  useEffect(() => {
+    const handleImageSaving = async () => {
+      const imageIds = [
+        "market-stats",
+        "performance",
+        "etf-tracker",
+        "stock-to-flow",
+        "interest-over-time",
+      ];
 
-  //     const images = await captureScreenshots(imageIds, "/api/saveImages");
+      const images = await captureScreenshots(imageIds, "/api/saveImages");
 
-  //     setImagesToDownload(
-  //       // @ts-ignore
-  //       images.reduce(
-  //         (acc, { fileName, image }) => ({
-  //           ...acc,
-  //           [fileName]: image,
-  //         }),
-  //         {}
-  //       )
-  //     );
-  //   };
+      setImagesToDownload(
+        // @ts-ignore
+        images.reduce(
+          (acc, { fileName, image }) => ({
+            ...acc,
+            [fileName]: image,
+          }),
+          {}
+        )
+      );
+    };
 
-  //   const imageSavingTimeout = setTimeout(() => {
-  //     handleImageSaving();
-  //   }, 2000);
+    const imageSavingTimeout = setTimeout(() => {
+      handleImageSaving();
+    }, 2000);
 
-  //   return () => {
-  //     clearTimeout(imageSavingTimeout);
-  //   };
-  // }, []);
+    return () => {
+      clearTimeout(imageSavingTimeout);
+    };
+  }, []);
 
   return (
-    // <div className="bitcoin-page">
-    //   <PriceTable
-    //     title="Bitcoin Price Data"
-    //     performance={performance}
-    //     marketStats={marketStats}
-    //   />
-    //   <ChartWrapper
-    //     legend
-    //     graphics
-    //     id="etf-tracker"
-    //     chartType={etfTrackerConfig}
-    //     data={etfData}
-    //     header="Etf Tracker"
-    //     subheader="Daily & Total To Date Inflows (Excluding Grayscale)"
-    //     share={shareLinks.etfTracker}
-    //     downloadImage={
-    //       imagesToDownload["etf-tracker" as keyof typeof imagesToDownload]
-    //     }
-    //   />
-    //   <HalvingView />
-    //   <ChartWrapper
-    //     data={stockToFlow}
-    //     id="stock-to-flow"
-    //     chartType={{
-    //       type: "stock-to-flow",
-    //     }}
-    //     header="Stock to Flow"
-    //     share={shareLinks.stockToFlow}
-    //     downloadImage={
-    //       imagesToDownload["stock-to-flow" as keyof typeof imagesToDownload]
-    //     }
-    //   />
-    //   <ChartWrapper
-    //     id="interest-over-time"
-    //     info={{
-    //       title: "Interest Over Time",
-    //       desc: "Google Trends data shows the relative popularity of search terms over time. Interest is represented on a scale from 0 to 100, where 100 is the most popular term.",
-    //     }}
-    //     chartType={interestChartConfig}
-    //     data={googleTrends}
-    //     header="Retail Interest"
-    //     share={shareLinks.interestOverTime}
-    //     downloadImage={
-    //       imagesToDownload[
-    //         "interest-over-time" as keyof typeof imagesToDownload
-    //       ]
-    //     }
-    //   />
-    // </div>
-    <div>
-
+    <div className="bitcoin-page">
+      <PriceTable
+        title="Bitcoin Price Data"
+        performance={performance}
+        marketStats={marketStats}
+      />
+      <ChartWrapper
+        legend
+        graphics
+        id="etf-tracker"
+        chartType={etfTrackerConfig}
+        data={etfData}
+        header="Etf Tracker"
+        subheader="Daily & Total To Date Inflows (Excluding Grayscale)"
+        share={shareLinks.etfTracker}
+        downloadImage={
+          imagesToDownload["etf-tracker" as keyof typeof imagesToDownload]
+        }
+      />
+      <HalvingView />
+      <ChartWrapper
+        data={stockToFlow}
+        id="stock-to-flow"
+        chartType={{
+          type: "stock-to-flow",
+        }}
+        header="Stock to Flow"
+        share={shareLinks.stockToFlow}
+        downloadImage={
+          imagesToDownload["stock-to-flow" as keyof typeof imagesToDownload]
+        }
+      />
+      <ChartWrapper
+        id="interest-over-time"
+        info={{
+          title: "Interest Over Time",
+          desc: "Google Trends data shows the relative popularity of search terms over time. Interest is represented on a scale from 0 to 100, where 100 is the most popular term.",
+        }}
+        chartType={interestChartConfig}
+        data={googleTrends}
+        header="Retail Interest"
+        share={shareLinks.interestOverTime}
+        downloadImage={
+          imagesToDownload[
+            "interest-over-time" as keyof typeof imagesToDownload
+          ]
+        }
+      />
     </div>
   );
 };
 
 export default PriceData;
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const etfData = await scrapeEtfFlows();
-//   const stockToFlow = await fetchStockToFlow();
-//   const performance = await getPerformanceStats("btc");
-//   const marketStats = await formattedMarketStats("btc");
-//   const googleTrends = await fetchGoogleTrends("bitcoin");
+export const getStaticProps: GetStaticProps = async () => {
+  const etfData = await scrapeEtfFlows();
+  const stockToFlow = await fetchStockToFlow();
+  const performance = await getPerformanceStats("btc");
+  const marketStats = await formattedMarketStats("btc");
+  const googleTrends = await fetchGoogleTrends("bitcoin");
 
-//   return {
-//     props: {
-//       stockToFlow: stockToFlow ?? [],
-//       marketStats: marketStats ?? [],
-//       googleTrends: googleTrends ?? [],
-//       etfData: etfData?.dailyFlows ?? [],
-//       performance: performance?.data ?? [],
-//     },
-//     revalidate: 43200,
-//   };
-// };
-
-
-// {
-//   etfData,
-//   marketStats,
-//   performance,
-//   stockToFlow,
-//   googleTrends,
-// }: PriceDataProps
+  return {
+    props: {
+      stockToFlow: stockToFlow ?? [],
+      marketStats: marketStats ?? [],
+      googleTrends: googleTrends ?? [],
+      etfData: etfData?.dailyFlows ?? [],
+      performance: performance?.data ?? [],
+    },
+    revalidate: 43200,
+  };
+};
--- src/pages/defi.tsx
+++ src/pages/defi.tsx
@@ -4,37 +4,32 @@ import DexTable from "@/components/common/DexTable";
 import { dexColumns, perpColumns } from "@/components/common/DexTable/columns";
 import { scrapePerp, scrapeExchanges } from "@/utils/scraper";
 
-const Defi = () => {
+const Defi = ({ dex, topPerp }: any) => {
   return (
-    // <div className="defi">
-    //   <h1>Defi Price Data</h1>
-    //   <p>Top 10 Cryptocurrency Decentralized Exchanges</p>
-    //   <DexTable title="DEX" data={dex} columns={dexColumns} />
-    //   <DexTable
-    //     title="Top 10 Richest Perpetual Protocol (PERP) Adresses"
-    //     data={topPerp}
-    //     columns={perpColumns}
-    //   />
-    // </div>
-    <div>
-      
+    <div className="defi">
+      <h1>Defi Price Data</h1>
+      <p>Top 10 Cryptocurrency Decentralized Exchanges</p>
+      <DexTable title="DEX" data={dex} columns={dexColumns} />
+      <DexTable
+        title="Top 10 Richest Perpetual Protocol (PERP) Adresses"
+        data={topPerp}
+        columns={perpColumns}
+      />
     </div>
   );
 };
 
 export default Defi;
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const dex = await scrapeExchanges();
-//   const topPerp = await scrapePerp();
+export const getStaticProps: GetStaticProps = async () => {
+  const dex = await scrapeExchanges();
+  const topPerp = await scrapePerp();
 
-//   return {
-//     props: {
-//       dex,
-//       topPerp,
-//     },
-//     revalidate: 43200,
-//   };
-// };
-
-// { dex, topPerp }: any
+  return {
+    props: {
+      dex,
+      topPerp,
+    },
+    revalidate: 43200,
+  };
+};
